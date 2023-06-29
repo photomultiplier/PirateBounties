acbb3cb141b4d32370b821d3fda572391b8ceb9c
photomultiplier <121574880+photomultiplier@users.noreply.github.com>
2023-06-29 11:09:42 +0200

    feat: add events for entering and leaving the leaderboard

--- src/main/java/com/github/photomultiplier/piratebounties/managers/EmperorsManager.java
+++ src/main/java/com/github/photomultiplier/piratebounties/managers/EmperorsManager.java
@@ -9,8 +9,10 @@ import java.util.Collection;
 import java.util.UUID;
 
 import com.github.photomultiplier.piratebounties.PirateBounties;
+import com.github.photomultiplier.piratebounties.utils.ActionsGroup;
 import com.github.photomultiplier.piratebounties.utils.Emperor;
 import com.github.photomultiplier.piratebounties.utils.EmperorLeaderboard;
+import com.github.photomultiplier.piratebounties.utils.ParamSubst;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -29,6 +31,8 @@ public abstract class EmperorsManager {
 	static int emperorAmount = 1;
 	static File dataFile;
 	static EmperorLeaderboard leaderBoard = new EmperorLeaderboard(4);
+	static ActionsGroup leaderboardEnter;
+	static ActionsGroup leaderboardLeave;
 
 	/**
 	 * Returns the minimum bounty needed to become an emperor.
@@ -65,6 +69,8 @@ public abstract class EmperorsManager {
 		FileConfiguration config = pg.getConfig();
 		emperorThreshold = config.getInt("general.emperorThreshold");
 		emperorAmount = config.getInt("general.emperorAmount");
+		leaderboardEnter = new ActionsGroup(config, "leaderboardEnter");
+		leaderboardLeave = new ActionsGroup(config, "leaderboardLeave");
 
 		File dataFolder = pg.getDataFolder();
 		if (!dataFolder.exists()) {
@@ -112,11 +118,19 @@ public abstract class EmperorsManager {
 	 * @param newEmperor The emperor to be updated.
 	 */
 	public static void updateSingle(Emperor newEmperor) {
-		UUID originalUUID = newEmperor.uuid;
+		Emperor og = newEmperor;
 		boolean seen = false;
 
+		// Four things may happen:
+		// 1. newEmperor gets added and the last emperor is removed as
+		// there wasn't space on the leaderboard
+		// 2. newEmperor gets added but no one is removed as there was
+		// space on the leaderboard
+		// 3. newEmperor gets removed
+		// 4. No one gets added or removed
+
 		for (int i = 0; i < leaderBoard.l.length; i++) {
-			if (seen || (leaderBoard.l[i] != null && originalUUID.equals(leaderBoard.l[i].uuid))) {
+			if (seen || (leaderBoard.l[i] != null && og.uuid.equals(leaderBoard.l[i].uuid))) {
 				if (i + 1 > emperorAmount) {
 					leaderBoard.l[i] = null;
 				} else {
@@ -128,19 +142,56 @@ public abstract class EmperorsManager {
 			if (leaderBoard.l[i] == null) {
 				if (newEmperor.bounty >= emperorThreshold) {
 					leaderBoard.l[i] = newEmperor;
+					newEmperor = null;
 				}
 				break;
 			} else {
 				if (newEmperor.bounty >= leaderBoard.l[i].bounty) {
 					if (seen) {
 						leaderBoard.l[i] = newEmperor;
+						newEmperor = null;
 						break;
 					} else {
 						Emperor buffer = leaderBoard.l[i];
 						leaderBoard.l[i] = newEmperor;
 						newEmperor = buffer;
+						if (newEmperor == null) {
+							break;
+						}
 					}
 				}
+			}
+		}
+
+		if (seen) {
+			// Surely it hasn't been added
+			// Cases 3 and 4
+			if (newEmperor != null) {
+				// Case 3
+				leaderboardLeave.execute(Bukkit.getPlayer(og.uuid),
+				                         new ParamSubst("player", og.displayName),
+				                         new ParamSubst("bounty", og.bounty));
+			} // else case 4, nothing to do
+		} else {
+			// Surely it hasn't been removed
+			// Cases 1, 2 and 4
+			if (newEmperor == null) {
+				// Surely it was added and surely everyone fit
+				// Case 2
+				leaderboardEnter.execute(Bukkit.getPlayer(og.uuid),
+				                         new ParamSubst("player", og.displayName),
+				                         new ParamSubst("bounty", og.bounty));
+			} else {
+				// Cases 1 and 4
+				if (!newEmperor.uuid.equals(og.uuid)) {
+					// Case 1
+					leaderboardEnter.execute(Bukkit.getPlayer(og.uuid),
+					                         new ParamSubst("player", og.displayName),
+					                         new ParamSubst("bounty", og.bounty));
+					leaderboardLeave.execute(Bukkit.getPlayer(newEmperor.uuid),
+					                         new ParamSubst("player", newEmperor.displayName),
+					                         new ParamSubst("bounty", newEmperor.bounty));
+				} // else case 4, nothing to do
 			}
 		}
 	}
---
+++ src/main/java/com/github/photomultiplier/piratebounties/utils/ActionsGroup.java
@@ -0,0 +1,87 @@
+// Copyright (C) 2023 photomultiplier
+// This program is licensed under the GNU General Public License.
+// Detailed licensing information is available in the "LICENSE" file.
+
+package com.github.photomultiplier.piratebounties.utils;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+/**
+ * Represents a group of actions: commands and messages.
+ */
+public class ActionsGroup {
+	/**
+	 * Is the action group enabled?
+	 */
+	public boolean enabled;
+	/**
+	 * A list of commands to execute.
+	 */
+	public List<String> commands;
+	/**
+	 * A message to send to the player.
+	 */
+	public String playerMessage;
+	/**
+	 * A message to broadcast to everyone.
+	 */
+	public String broadcastMessage;
+
+	/**
+	 * Constructs an ActionsGroup from raw parameters.
+	 *
+	 * @param enabled Is the action group enabled?
+	 * @param commands A list of commands to execute.
+	 * @param playerMessage A message to send to the player.
+	 * @param broadcastMessage A message to broadcast to everyone.
+	 */
+	public ActionsGroup(boolean enabled, List<String> commands, String playerMessage, String broadcastMessage) {
+		this.enabled = enabled;
+		this.commands = commands;
+		this.playerMessage = playerMessage;
+		this.broadcastMessage = broadcastMessage;
+	}
+
+	/**
+	 * Constructs an ActionsGroup from a configuration file and event name.
+	 *
+	 * @param config The {@link FileConfiguration}.
+	 * @param eventName The name of the event.  All parameters will be
+	 * searched in "events.&lt;eventName&gt;".
+	 */
+	public ActionsGroup(FileConfiguration config, String eventName) {
+		String path = "events." + eventName;
+		this.enabled = config.getBoolean(path + ".enabled");
+		this.commands = config.getStringList(path + ".commands");
+		this.playerMessage = TextUtils.msgFromConfig(config.getStringList(path + ".playerMessage"));
+		this.broadcastMessage = TextUtils.msgFromConfig(config.getStringList(path + ".broadcastMessage"));
+	}
+
+	/**
+	 * Execute the commands as the console and broadcast messages to whoever necessary.
+	 *
+	 * Note that the substitutions are applied to commands and messages alike.
+	 *
+	 * @param player The player to send messages to.  Can be null.
+	 * @param substitutions The list of substitutions.
+	 */
+	public void execute(Player player, ParamSubst... substitutions) {
+		if (enabled) {
+			if (commands != null) {
+				for (String command : commands) {
+					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), TextUtils.replace(command, substitutions));
+				}
+			}
+			if (playerMessage != null && player != null) {
+				player.sendMessage(TextUtils.replace(playerMessage, substitutions));
+			}
+			if (broadcastMessage != null) {
+				Bukkit.getServer().broadcastMessage(TextUtils.replace(broadcastMessage, substitutions));
+			}
+		}
+	}
+}
